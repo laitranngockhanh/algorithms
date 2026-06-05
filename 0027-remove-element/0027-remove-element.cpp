ebacd840d09fea29d24eb37cadee37480a81c517
Lai Tran Ngoc Khanh <khanhlai6420@gmail.com>
2026-06-05 23:02:25 +0700

    Time: 0 ms (100%), Space: 11.7 MB (83.34%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -6,13 +6,14 @@ public:
             return 0;
         }
         int k = 0;
-        for(int i = 0 ;i< nums.size();i++)
+        for(int i = 0 ;i<nums.size();i++)
         {
             if(nums[i]!=val)
             {
-                nums[k]= nums[i];
+                nums[k]=nums[i];
                 k++;
             }
-        }return k;
+        }
+        return k;
     }
 };
